ffcbe8c484dac4dc5f380e5ab195e4e1df37bcbe
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2013-12-19 23:45:59 +0700

    Add example for processing command response containing events e.g. queuestatus command

--- src/index.js
+++ src/index.js
@@ -91,6 +91,81 @@ nami.on('namiConnected', function (event) {
     action = new namiLib.Actions.GetConfigJson();
     action.filename = "sip.conf";
     standardSend(action);
+
+	action = new namiLib.Actions.QueueStatus();
+	nami.send(action, function (response) {
+		var c = 0;
+		var queues = new Array();
+		if (response.events !== 'undefined')
+		{
+			c = response.events.length;
+		}
+		logger.debug('Got response for QueueStatus command bundled with %d events', c);	
+		
+		for (var x=0; x < c ; x++)
+		{
+			event = response.events[x];
+			if (event.event == 'QueueParams')
+			{
+				var q = new Object();
+				q.name = event.queue;
+				q.max = event.max;
+				q.strategy = event.strategy;
+				q.calls	= event.calls;
+				q.holdtime = event.holdtime;
+				q.talktime = event.talktime;
+				q.completed = event.completed;
+				q.abandoned = event.abandoned;
+				q.servicelevel = event.servicelevel;
+				q.servicelevelperf = event.servicelevelperf;
+				q.members = new Array();
+				q.calls = new Array();
+
+				queues.push(q);
+			}
+			if (event.event == 'QueueMemeber')
+			{
+				var agent = new Object();
+				agent.name = event.name;
+				agent.callstaken = event.callstaken;
+				agent.lastcall = event.lastcall;
+				agent.status = event.status;
+				agent.paused = event.paused;
+				
+				for (var i=0; i < queues.length; i++ )
+				{
+					if (queues[i].name == event.queue)
+					{
+						queues[i].memebers.push(agent);
+						break;
+					}
+				}
+
+			}
+			if (event.event == 'QueueEntry')
+			{
+				var call = new Object();
+				call.callerid = event.callerid;
+				call.callername = event.callername;
+				call.channel = event.channel;
+				call.wait = event.wait;
+				
+				for (var i=0; i < queues.length; i++ )
+				{
+					if (queues[i].name == event.queue)
+					{
+						queues[i].calls.push(agent);
+						break;
+					}
+				}
+
+			}
+			
+			logger.debug('Event got ' + event.event);
+		}
+
+		logger.debug('Got data ' + util.inspect(queues)); 
+	});
 });
 nami.open();
 
